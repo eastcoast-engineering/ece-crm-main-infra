c885eeef24ed05ff38833e181f27e525165d705d
tom.kidumbuyo <tom.kidumbuyo@gmail.com>
2026-08-02 07:38:22 +0300

    chore(ECR): made ecer mutable

--- terraform/modules/backend_deployment/ecr.tf
+++ terraform/modules/backend_deployment/ecr.tf
@@ -1,7 +1,12 @@
 resource "aws_ecr_repository" "api" {
   name                 = "${local.name_prefix}-api"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
   force_delete         = var.ecr_force_delete
+
+  image_tag_mutability_exclusion_filter {
+    filter_type = "WILDCARD"
+    filter      = "buildcache"
+  }
 
   image_scanning_configuration {
     scan_on_push = true
